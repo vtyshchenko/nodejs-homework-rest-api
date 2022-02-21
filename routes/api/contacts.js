05eac450c7485f689c156a5236cbbbd8cf7b3146
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2022-02-21 22:19:29 +0200

    add validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jois = require('joi');
 
 const router = express.Router()
 
@@ -11,8 +12,7 @@ const contactsList = require("../../helpers/contacts");
 router.get('/', async (req, res, next) => {
   const contacts = await contactsList.listContacts();
   console.table(contacts);
-  res.json({status: "accepted", status_code: 200, data: contacts })
-  // next();
+  res.json({ status: "accepted", status_code: 200, data: contacts })
 })
 
 // @ GET /api/contacts/:id
@@ -28,7 +28,6 @@ router.get('/:contactId', async (req, res, next) => {
   } else {
     res.json({ status: "accepted", status_code: 200, data: contact })
   }
-  // next();
 })
 
 // @ POST /api/contacts
@@ -39,24 +38,32 @@ router.get('/:contactId', async (req, res, next) => {
 //     По результату работы функции возвращает объект с добавленным id {id, name, email, phone} и статусом 201
 router.post('/', async (req, res, next) => {
   const { name, email, phone } = req.body
-  
+
   let err = !name ? 'name,' : ''
   err += !email ? 'email,' : ''
   err += !phone ? 'phone' : ''
   err = err && err.slice(-1) === ',' ? err.slice(0, err.length - 1) : err
-  
+
   if (err) {
     res.json({ status: "rejected", status_code: 400, message: `missing required ${err} field` })
-  }
-  else {
-    const addedContact = await contactsList.addContact(name, email, phone);
-    if (!addedContact) {
-      res.json({ status: "rejected", status_code: 410, message: "Error add" })
-    } else {
-      res.json({ status: "accepted", status_code: 201, data: addedContact })
+  } else {
+    try {
+      const validate = await validateData({ name, email, phone });
+      if (validate.res) {
+        const addedContact = await contactsList.addContact(name, email, phone);
+        if (!addedContact) {
+          res.json({ status: "rejected", status_code: 410, message: "Error adding data" })
+        } else {
+          res.json({ status: "accepted", status_code: 201, data: addedContact })
+        }
+      } else {
+        res.json({ status: "rejected", status_code: 409, message: `Wrong value of required fields: ${validate.error.message}` })
+      }
+    }
+    catch (error) {
+      res.json({ status: "rejected", status_code: 409, message: error.message })
     }
   }
-  // next()
 })
 
 // @ DELETE /api/contacts/:id
@@ -68,11 +75,10 @@ router.post('/', async (req, res, next) => {
 router.delete('/:contactId', async (req, res, next) => {
   const removedContact = await contactsList.removeContact(req.params.contactId);
   if (!removedContact) {
-      res.json({ status: "rejected", status_code: 404, message: "Not found" })
-    } else {
-      res.json({ status: "accepted", status_code: 200, message: "contact deleted" })
-    }
-  // next();
+    res.json({ status: "rejected", status_code: 404, message: "Not found" })
+  } else {
+    res.json({ status: "accepted", status_code: 200, message: "contact deleted" })
+  }
 })
 
 // @ PUT /api/contacts/:id
@@ -88,15 +94,45 @@ router.put('/:contactId', async (req, res, next) => {
     res.json({ status: "rejected", status_code: 400, message: "missing fields" })
   } else {
     const { name, email, phone } = req.body
-    const updatedContact = await contactsList.updateContact(req.params.contactId, name, email, phone);
-    if (!updatedContact) {
-      res.json({ status: "rejected", status_code: 404, message: "Not found" })
+    const validate = await validateData({ name, email, phone });
+
+    if (validate.res) {
+      const updatedContact = await contactsList.updateContact(req.params.contactId, name, email, phone);
+      if (!updatedContact) {
+        res.json({ status: "rejected", status_code: 404, message: "Not found" })
+      } else {
+        res.json({ status: "accepted", status_code: 200, data: updatedContact })
+      }
     } else {
-      res.json({ status: "accepted", status_code: 200, data: updatedContact })
-    }
+        res.json({ status: "rejected", status_code: 409, message: `Wrong value of required fields: ${validate.error.message}` })
+      }
   }
-  // next()
 })
 
-module.exports = router
+async function validateData({ name, email, phone }) {
+  const schema = jois.object({
+    name: jois.string()
+      .min(3)
+      .max(35)
+      .required()
+    ,
+    email: jois.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua'] } }).required()
+    ,
+    phone: jois.string().pattern(
+      /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+      "number"
+    )
+      .required()
+  })
+  try {
+    const validate = await schema.validateAsync({ name, email, phone });
+    console.log('validate', validate);
+    return { res: true, error: '' };
+  }
+  catch (error) {
+    return { res: false, error };
+  }
+}
 
+module.exports = router
