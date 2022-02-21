335f6af5e66eb7301ec6d26daf4739f5978e2db8
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2022-02-21 23:29:13 +0200

    all done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,22 +5,12 @@ const router = express.Router()
 
 const contactsList = require("../../helpers/contacts");
 
-// @ GET /api/contacts
-//     ничего не получает
-//     вызывает функцию listContacts для работы с json-файлом contacts.json
-//     возвращает массив всех контактов в json-формате со статусом 200
 router.get('/', async (req, res, next) => {
   const contacts = await contactsList.listContacts();
   console.table(contacts);
   res.json({ status: "accepted", status_code: 200, data: contacts })
 })
 
-// @ GET /api/contacts/:id
-//     Не получает body
-//     Получает параметр id
-//     вызывает функцию getById для работы с json-файлом contacts.json
-//     если такой id есть, возвращает объект контакта в json-формате со статусом 200
-//     если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
 router.get('/:contactId', async (req, res, next) => {
   const contact = await contactsList.getContactById(req.params.contactId);
   if (!contact) {
@@ -30,12 +20,6 @@ router.get('/:contactId', async (req, res, next) => {
   }
 })
 
-// @ POST /api/contacts
-//     Получает body в формате {name, email, phone} (все поля обязательны)
-//     Если в body нет каких-то обязательных полей, возвращает json с ключом {"message": "missing required name field"} и статусом 400
-//     Если с body все хорошо, добавляет уникальный идентификатор в объект контакта
-//     Вызывает функцию addContact(body) для сохранения контакта в файле contacts.json
-//     По результату работы функции возвращает объект с добавленным id {id, name, email, phone} и статусом 201
 router.post('/', async (req, res, next) => {
   const { name, email, phone } = req.body
 
@@ -66,12 +50,6 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-// @ DELETE /api/contacts/:id
-//     Не получает body
-//     Получает параметр id
-//     вызывает функцию removeContact для работы с json-файлом contacts.json
-//     если такой id есть, возвращает json формата {"message": "contact deleted"} и статусом 200
-//     если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
 router.delete('/:contactId', async (req, res, next) => {
   const removedContact = await contactsList.removeContact(req.params.contactId);
   if (!removedContact) {
@@ -81,15 +59,7 @@ router.delete('/:contactId', async (req, res, next) => {
   }
 })
 
-// @ PUT /api/contacts/:id
-//     Получает параметр id
-//     Получает body в json-формате c обновлением любых полей name, email и phone
-//     Если body нет, возвращает json с ключом {"message": "missing fields"} и статусом 400
-//     Если с body все хорошо, вызывает функцию updateContact(contactId, body) (напиши ее) для обновления контакта в файле contacts.json
-//     По результату работы функции возвращает обновленный объект контакта и статусом 200. В противном случае, возвращает json с ключом "message": "Not found" и статусом 404
 router.put('/:contactId', async (req, res, next) => {
-  console.log(req.params.contactId);
-  console.log(req.body);
   if (!req.body) {
     res.json({ status: "rejected", status_code: 400, message: "missing fields" })
   } else {
@@ -126,8 +96,7 @@ async function validateData({ name, email, phone }) {
       .required()
   })
   try {
-    const validate = await schema.validateAsync({ name, email, phone });
-    console.log('validate', validate);
+    await schema.validateAsync({ name, email, phone });
     return { res: true, error: '' };
   }
   catch (error) {
